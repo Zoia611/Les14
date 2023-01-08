a9bea9aaaf8f527c8bccb542e7b60ad2c39a62a2
Zoia611 <117438266+Zoia611@users.noreply.github.com>
2023-01-08 14:08:56 +0300

    Добавьте файлы проекта.

---
+++ Task1/Converter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+
+namespace Task1
+{
+    internal class Converter VelueConverter
+        public object Convert(object velue, Type targetType, object parameter, CultureInfo culture)
+        {
+        if ((ProductCategorys)value == ProductCategories.Food)
+         {
+            return "Еда";
+         }
+        return "Бытовая техника";
+        }
+        public object ConvertBack(object velue, Type targetType, object parameter, CultureInfo culture)
+     {
+        return DependencyProperty.UnsetValue;
+     }
+    }
+
---
+++ Task1/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new ObservableCollection<Product>();
+            products.Add(new Product()
+            {
+                ProductName = "Соленые огурцы",
+                ProductPrice = 100,
+                ProductImage = "Data\\images1.jpg",
+                ProductCategory = ProductCategories.Food
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Молоко",
+                ProductPrice = 60,
+                ProductImage = "Data\\images2.jpg",
+                ProductCategory = ProductCategories.Food
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Холодильник",
+                ProductPrice = 35000,
+                ProductImage = "Data\\images3.jpg",
+                ProductCategory = ProductCategories.Appliances
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Микроволновая печь",
+                ProductPrice = 5000,
+                ProductImage = "Data\\images4.jpg",
+                ProductCategory = ProductCategories.Appliances
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Рыба",
+                ProductPrice = 300,
+                ProductImage = "Data\\images5.jpg",
+                ProductCategory = ProductCategories.Food
+            });
+            lstBox.ItemsSource = products;
+        }
+    }
+}
+
+
